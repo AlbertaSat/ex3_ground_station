6d6cf0afae08ce3e52d29c4a8b6dcdeb7cf29417
waridh <waridh@ualberta.ca>
2024-03-30 22:05:09 -0600

    added new module files

---
+++ ex3_gnd_stn/src/connector.rs
@@ -0,0 +1,4 @@
+use std::{
+    io::{self, Write},
+    net::TcpStream,
+};
--- ex3_gnd_stn/src/main.rs
+++ ex3_gnd_stn/src/main.rs
@@ -1,20 +1,32 @@
-use std::{
-    io::{self, Write},
-    net::TcpStream,
-};
+use cursive::views::{Dialog, TextView};
+
+mod connector;
+mod generator;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut stream = TcpStream::connect("127.0.0.1:8080")?;
-    let mut data: [u8; 64] = [0; 64];
+    // let mut stream = TcpStream::connect("127.0.0.1:8080")?;
+    // let mut data: [u8; 64] = [0; 64];
 
-    // The first three bytes are header stuff, the rest will be payload
-    // We are just trying to send to ADCS right now
+    // // The first three bytes are header stuff, the rest will be payload
+    // // We are just trying to send to ADCS right now
 
-    data[0] = 64;
-    data[1] = 3;
-    data[2] = 69;
+    // data[0] = 64;
+    // data[1] = 3;
+    // data[2] = 69;
 
-    let x = stream.write(&data)?;
-    println!("{x}");
+    // let x = stream.write(&data)?;
+    // println!("{x}");
+
+    // This is really a place to setup the tui
+    let mut siv = cursive::default();
+
+    siv.add_global_callback('q', |s| s.quit());
+
+    siv.add_layer(
+        Dialog::around(TextView::new("Hello AlbertaSat"))
+            .title("Ground Station")
+            .button("Quit", |s| s.quit()),
+    );
+    siv.run();
     Ok(())
 }
