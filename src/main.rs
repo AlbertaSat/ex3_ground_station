b66aace415f50c77eb006477461fa67fb6e781b9
iniadeniyi <inioluwa@ualberta.ca>
2024-06-17 10:24:20 -0600

    feat: created the Command entity, and implemented functionality to save the command to the database on receiving the command

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,83 @@
 use crate::obc_client::ObcClient;
-
-#[macro_use] extern crate rocket;
-
+use sea_orm::{Database, DatabaseConnection, EntityTrait, Set, ActiveModelTrait};
 use rocket::fs::{relative, FileServer};
 use rocket::serde::{Deserialize, json::Json};
+use rocket::response::status;
+use rocket::State;
 use tokio::sync::Mutex;
 use once_cell::sync::Lazy;
 use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
+use rocket::http::Status;
+use entities::command;
 
+#[macro_use]
+extern crate rocket;
 
 mod obc_client;
 mod message;
+mod entities;
 
 static OBC_CLIENT: Lazy<Mutex<ObcClient>> = Lazy::new(|| Mutex::new(
-                            ObcClient::new("localhost".to_string(), 50000)
-                                                        ));
+    ObcClient::new("localhost".to_string(), 50000)
+));
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
-struct WebCommand<'r> {
+struct CommandInput<'r> {
     payload: &'r str,
     cmd: &'r str,
     data: &'r str,
+    timestamp: &'r str,
 }
 
 #[post("/api/cmd", format = "json", data = "<input>")]
-async fn webcmd(input: Json<WebCommand<'_>>) {
-    println!("got a form! payload {}, op {}, data {}", input.payload, input.cmd, input.data);
+async fn post_cmd(connection: &State<DatabaseConnection>, input: Json<CommandInput<'_>>) {
+    println!("Got a form! Payload: {}, Cmd: {}, Data: {}", input.payload, input.cmd, input.data);
+
+    let new_command = command::ActiveModel {
+        command: Set(input.cmd.to_string()),
+        payload: Set(input.payload.to_string()),
+        data: Set(input.data.to_string()),
+        timestamp: Set(input.timestamp.to_string()),
+        ..Default::default()
+    };
+
+    new_command.save(connection.inner()).await.expect("Could not save command");
+
     let mut client = OBC_CLIENT.lock().await;
     match client.send_cmd([input.payload, input.cmd, input.data]).await {
-        Ok(rc) => println!("client response {}", rc),
-        Err(e) => println!("client send err {}", e),
+        Ok(rc) => println!("Client response: {}", rc),
+        Err(e) => println!("Client send error: {}", e),
     };
 }
 
 #[options("/api/cmd")]
-fn options_cors() -> rocket::http::Status {
-    rocket::http::Status::Ok
+fn options_cors() -> Status {
+    Status::Ok
 }
 
 #[launch]
 async fn rocket() -> _ {
+    let connection = match Database::connect("postgresql://inioluwaadeniyi:5678@localhost:5432/ex_alta_3").await {
+        Ok(connection) => connection,
+        Err(e) => panic!("Error connecting to database: {}", e),
+    };
+
     let mut client = OBC_CLIENT.lock().await;
     match client.connect().await {
-        Ok(_) => println!("connected to obc"),
-        Err(e) => println!("connection error: {}", e),
+        Ok(_) => println!("Connected to OBC"),
+        Err(e) => println!("Connection error: {}", e),
     }
 
     let cors = CorsOptions::default()
-    .allowed_origins(AllowedOrigins::all())
-    .allowed_headers(AllowedHeaders::all())
-    .allow_credentials(true);
+        .allowed_origins(AllowedOrigins::all())
+        .allowed_headers(AllowedHeaders::all())
+        .allow_credentials(true)
+        .to_cors().expect("Error creating CORS options");
 
     rocket::build()
-        .mount("/", routes![webcmd, options_cors])
+        .manage(connection)
+        .mount("/", routes![post_cmd, options_cors])
         .mount("/", FileServer::from(relative!("static")))
-        .attach(cors.to_cors().unwrap())
+        .attach(cors)
 }
-
